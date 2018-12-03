a1ef009870df76859c2e391788cd432c3de4d45e
Marlon Fan <marlon@tfan.net>
2018-12-03 14:51:13 +0800

    fix link nvim config bug

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 ln -s "${HOME}/vim-git/.vimrc" "${HOME}/.vimrc"
 ln -s "${HOME}/vim-git/.vim" "${HOME}/.vim"
 
-if [ ! -d "$HOME/.config/nvim" ]
+if [ ! -d "$HOME/.config" ]
 then 
-    mkdir "${HOME}/.config/nvim"
+    mkdir "${HOME}/.config"
 fi
 
-ln -s "${HOME}/vim-git/.vim" "${HOME}/.config/nvim/init.vim"
+ln -s "${HOME}/vim-git/.vim" "${HOME}/.config/nvim"
 ln -s "${HOME}/vim-git/.vimrc" "${HOME}/.config/nvim/init.vim"
 
