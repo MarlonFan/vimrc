e48b1d2ecbe0bf9b692b33893623010816dc7b17
Marlon Fan <marlon@tfan.net>
2019-01-09 22:11:13 +0800

    fix install script

--- install.sh
+++ install.sh
@@ -7,4 +7,5 @@ curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 ln -s "$(pwd)/.vimrc" "${HOME}/.config/nvim/init.vim"
+ln -s "$(pwd)/coc-settings.json" "${HOME}/.config/nvim/coc-settings.json"
 
